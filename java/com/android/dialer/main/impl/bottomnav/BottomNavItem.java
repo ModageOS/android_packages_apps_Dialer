2cee9f984864db31dc9541c30e475b3d2452068d
RahulPalXDA <RahulPal.XDA@gmail.com>
2023-07-23 20:40:03 +0530

    Revert "Dialer: use black/white navigation colors" - use accent colors This reverts commit 1854a4fcb5189545043f32e0fbe77b3b37f644aa.

--- java/com/android/dialer/main/impl/bottomnav/BottomNavItem.java
+++ java/com/android/dialer/main/impl/bottomnav/BottomNavItem.java
@@ -56,8 +56,8 @@ final class BottomNavItem extends LinearLayout {
     super.setSelected(selected);
     int colorId =
         selected
-            ? getContext().getResources().getColor(R.color.nav_item_selected)
-            : getContext().getResources().getColor(R.color.nav_item);
+            ? ThemeComponent.get(getContext()).theme().getColorPrimary()
+            : ThemeComponent.get(getContext()).theme().getTextColorSecondary();
     image.setImageTintList(ColorStateList.valueOf(colorId));
     text.setTextColor(colorId);
   }
